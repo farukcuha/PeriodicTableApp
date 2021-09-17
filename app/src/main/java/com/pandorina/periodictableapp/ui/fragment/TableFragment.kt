94ecafda240d17af437a307916bfa02eb1bafc74
farukcuha <ahmetfarukcuha@hotmail.com>
2021-09-17 17:54:53 +0300

    simple changes

--- app/src/main/java/com/pandorina/periodictableapp/ui/fragment/TableFragment.kt
+++ app/src/main/java/com/pandorina/periodictableapp/ui/fragment/TableFragment.kt
@@ -29,7 +29,7 @@ class TableFragment : BaseFragment<FragmentTableBinding>(FragmentTableBinding::i
     }
 
     private fun initPeriods(binding: FragmentTableBinding) {
-        val periodList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11)
+        val periodList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 
         binding.rvPeriods.apply {
             setHasFixedSize(true)
